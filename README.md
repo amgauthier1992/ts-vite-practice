6f49a4638a5ec1d7db0c0af2ffa7b6c6f111231f
Andy <amgauthier1992@gmail.com>
2024-04-03 11:43:54 -0400

    cleans up boilerplate files and adds models for List and ListItem

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Vite and TS Practice
+# Vite and TS Practice - Dave Gray TS Course (Lessons 10 & 11)
 
 - Vite is a local development server written by Evan You, the creator of Vue.js, and used by default by Vue and for React project templates. It has support for TypeScript and JSX. It uses Rollup and esbuild internally for bundling.
 
@@ -8,4 +8,7 @@
 
 1. npm create vite@latest
 2. Name project, select framework (vanilla, React, etc.), select variant (JS/TS)
-    
+3. npm install
+4. npm run dev
+
+- vite-env.d.ts : this is where we set types for env variables (see Vite docs for how to handle env vars)
---
+++ src/model/FullList.ts
@@ -0,0 +1,56 @@
+import ListItem from './ListItem';
+
+interface List {
+  list: ListItem[];
+  load(): void;
+  save(): void;
+  clear(): void;
+  addItem(itemObj: ListItem): void;
+  removeItem(id: string): void;
+}
+
+export default class FullList implements List {
+  // Private static property to hold the single instance - we know there's only going to be one list, so we can 
+  static instance: FullList = new FullList();
+
+  // Private constructor to prevent external instantiation - the combo of this and the above creates a singleton pattern. 
+  // This ensures one instance of the FullList class can be created. 
+  private constructor(private _list: ListItem[] = []) {}
+
+  get list(): ListItem[] {
+    return this._list;
+  }
+
+  load(): void {
+    const storedList: string | null = localStorage.getItem('myList');
+    if (typeof storedList !== 'string') {
+      return;
+    }
+
+    const parsedList: { _id: string; _item: string; _checked: boolean }[] = JSON.parse(storedList);
+
+    parsedList.forEach((listItem) => {
+      const newItem = new ListItem(listItem._id, listItem._item, listItem._checked);
+      FullList.instance.addItem(newItem);
+    });
+  }
+
+  save(): void {
+    localStorage.setItem('myList', JSON.stringify(this._list));
+  }
+
+  clear(): void {
+    this._list = [];
+    this.save();
+  }
+
+  addItem(item: ListItem): void {
+    this._list.push(item);
+    this.save();
+  }
+
+  removeItem(id: string): void {
+    this._list = this._list.filter((item) => item.id !== id);
+    this.save();
+  }
+}
